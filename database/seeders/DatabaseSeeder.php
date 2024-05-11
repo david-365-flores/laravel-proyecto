73ae563c1668c6596b6b1190293756685fc259d0
deibi <david.flores7341@alumnos.udg.mx>
2024-05-11 13:25:21 -0600

    ya le se a los seeders y factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Pelicula;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'david',
+            'email' => 'david@mail.com',
+            'password' => 'gatitos23'
         ]);
+        Pelicula::factory(15)->create();
     }
 }
